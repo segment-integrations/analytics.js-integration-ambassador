1bd2e2cedf8e25315ea9b489e1c560cb4b89ac56
Ivan Sugerman <ivan@getambassador.com>
2016-04-25 16:05:46 -0400

    added base structure

---
+++ test/index.test.js
@@ -0,0 +1,54 @@
+
+var Analytics = require('analytics.js-core').constructor;
+var integration = require('analytics.js-integration');
+var sandbox = require('clear-env');
+var tester = require('analytics.js-integration-tester');
+var Ambassador = require('../lib/');
+
+describe('Ambassador', function() {
+  var analytics;
+  var Ambassador;
+  var options = {
+    uid: '<PLACE_TEST_UID_HERE>'
+  };
+
+  beforeEach(function() {
+    analytics = new Analytics();
+    ambassador = new Ambassador(options);
+    analytics.use(Ambassador);
+    analytics.use(tester);
+    analytics.add(ambassador);
+  });
+
+  afterEach(function() {
+    analytics.restore();
+    analytics.reset();
+    ambassador.reset();
+    sandbox();
+  });
+
+  it('should have the right settings', function() {
+    analytics.compare(Ambassador, integration('Ambassador')
+      .global('_mbsy')
+      .option('uid', ''));
+  });
+
+  describe('before loading', function() {
+    beforeEach(function() {
+      analytics.stub(ambassador, 'load');
+    });
+
+    describe('#initialize', function() {
+      it('should create the window._mbsy object', function() {
+        analytics.assert(!window._mbsy);
+        analytics.initialize();
+        analytics.assert(window._mbsy);
+      });
+
+      it('should call #load', function() {
+        analytics.initialize();
+        analytics.called(Ambassador.load);
+      });
+    });
+  });
+});
